d0a8dfbb30d82051218b5b6dd2f3617c25ba00fb
Matteo Ricci <m.ricci@unibo.it>
2014-07-09 16:24:59 +0200

    corretto bug visualizzazione

--- include/classes/DAO/UsersDAO.class.php
+++ include/classes/DAO/UsersDAO.class.php
@@ -383,13 +383,14 @@ class UsersDAO extends DAO {
 	public function getUserName($userID)
 	{
                 global $_shib_user_identifier;
+                global $profile_user;
 		$row = $this->getUserByID($userID);
 		
 		if (!$row) return false;
 		
 		if (AUTH_METHOD == 'SHIB') {
                     
-                return $_SERVER[$_shib_user_identifier];
+                return $row['login'];
                 
                
             }else {

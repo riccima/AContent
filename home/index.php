11350ac8327775ee58532d32dc7283c13e32a95b
Matteo Ricci <m.ricci@unibo.it>
2014-08-28 14:04:29 +0200

    introdotto tema standard che diventa tema di defautl quando si esporta e corretto esportazione senza tema

--- home/index.php
+++ home/index.php
@@ -8,7 +8,7 @@
 /* This program is free software. You can redistribute it and/or        */
 /* modify it under the terms of the GNU General Public License          */
 /* as published by the Free Software Foundation.                        */
-/************************************************************************/
+/*******************y*****************************************************/
 
 define('TR_INCLUDE_PATH', '../include/');
 require(TR_INCLUDE_PATH.'vitals.inc.php');
--- templates/system/Layout.class.php
+++ templates/system/Layout.class.php
@@ -411,7 +411,7 @@ class Layout{
                 
 		for($i=0; $i < count($rows); $i++){
                         
-                        if($rows[$i]['layout'] == '' || !$this->exist_layout($rows[$i]['layout'])) $rows[$i]['layout'] = 'unibo';
+                        if($rows[$i]['layout'] == '' || !$this->exist_layout($rows[$i]['layout'])) $rows[$i]['layout'] = 'standard';
 			if($rows[$i]['layout'] != '' && $this->exist_layout($rows[$i]['layout'])){
 				// In another version, AContent requires 'commoncartridge' as folder
 				$rows[$i]['head']					= '<link rel="stylesheet" href="commoncartridge/'.$rows[$i]['layout'].'.css" type="text/css" />'.$rows[$i]['head'];
@@ -434,11 +434,15 @@ class Layout{
 							$zipfile->add_file($stylesheet, 'resources/commoncartridge/'.$rows[$i]['layout'].'.css');
 
 							// add images folder
-							$src	= '../../templates/layout/'.$rows[$i]['layout'].'/'.$rows[$i]['layout'].'/';
-							$dst	= 'resources/commoncartridge/'.$rows[$i]['layout'].'/';
-	
-							$zipfile->create_dir('resources/commoncartridge/'.$rows[$i]['layout'].'/');
-							$zipfile->add_dir($src, $dst);
+                                                        if ($rows[$i]['layout'] != 'nothing' && $rows[$i]['layout'] != 'standard' ) {
+                                                            $src = '../../templates/layout/'.$rows[$i]['layout'].'/'.$rows[$i]['layout'].'/'; 
+                                                            $dst	= 'resources/commoncartridge/'.$rows[$i]['layout'].'/';
+                                                            $zipfile->add_dir($src, $dst);
+                                                            $zipfile->create_dir('resources/commoncartridge/'.$rows[$i]['layout'].'/');
+                                                        }
+                                                      
+							
+							
 						}
 					}
 				}

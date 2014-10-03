9ca768c11fb306e8f0b7c437c3ea6a005ffb8ed7
Matteo Ricci <m.ricci@unibo.it>
2014-10-03 14:34:54 +0200

    sistemato (forse) comparsa errore messaggio login (svista clamorosa)

--- shibboleth/include/classes/DAO/ShibUsersDAO.class.php
+++ shibboleth/include/classes/DAO/ShibUsersDAO.class.php
@@ -208,17 +208,17 @@ class ShibUsersDAO extends UsersDAO {
         
         public function isUserFieldsMissing($userID)
 	{
-		
-                if (!$user['login']) {
+		 
+                $user = $this->getUserByID($userID);   
+                
+                 if (!$user['login']) {
                     
                     $user['login'] = '';
                     $login_name= substr($_SERVER['eppn'],0, strpos($_SERVER['eppn'],'@'));
                     $user['login'] = $login_name ;
                 }
-                
-                
-                $user = $this->getUserByID($userID);                                
-		if($this->isFieldsValid($userID, $user['user_group_id'], $user['login'], $user['email'], $user['first_name'], $user['last_name'],
+                   
+                if($this->isFieldsValid($userID, $user['user_group_id'], $user['login'], $user['email'], $user['first_name'], $user['last_name'],
 		                        $user['is_author'], $user['organization'], $user['phone'], $user['address'], $user['city'],
 	                            $user['province'], $user['country'], $user['postal_code']))
 			return false;
